d07437c87a6f4426d9e9e0f57069f8a608bef837
Tomoya Takiwaki <takiwaki.tomoya@nao.ac.jp>
2022-11-27 15:51:59 +0900

    update

--- HYD3D/CountBindata.sh
+++ HYD3D/CountBindata.sh
@@ -21,6 +21,8 @@ if [ ! -d ${dir} ]; then
     mkdir ${dir}
 fi
 
+echo "count files in bindata"
+
 fstfile=`ls -1 bindata/unf*.dat 2>/dev/null | head -1`
 echo $fstfile
 declare -i fstnum=`echo  ${fstfile##*/} | tr -cd '0123456789\n' |sed -e 's/^0\+\([0-9]\+\)$/\1/'`
@@ -28,6 +30,8 @@ declare -i fstnum=`echo  ${fstfile##*/} | tr -cd '0123456789\n' |sed -e 's/^0\+\
 lstfile=`ls -1 bindata/unf*.dat 2>/dev/null | tail -1`
 echo $lstfile
 declare -i lstnum=`echo  ${lstfile##*/} | tr -cd '0123456789\n' |sed -e 's/^0\+\([0-9]\+\)$/\1/'`
+
+echo "count is save in control.dat"
 
 echo ${fstnum} ${lstnum} > tmp
 cat tmp | tr -d \\n  > control.dat
