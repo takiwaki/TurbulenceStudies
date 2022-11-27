28056e05e6553718658bc0b48b7a923caafe640d
Tomoya Takiwaki <takiwaki.tomoya@nao.ac.jp>
2022-11-27 20:07:48 +0900

    update

---
+++ MHD2Dgpu/sj_g00_ana.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+#SBATCH --partition=ga80-1gpu
+#SBATCH --gres=gpu:1
+
+# usage sbatch sj_g00.sh
+# other useful commands
+# sinfo
+# squeue
+
+module load nvhpc
+./Analysis.x > logana.dat
