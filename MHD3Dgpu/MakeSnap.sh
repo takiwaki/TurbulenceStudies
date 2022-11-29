14be779ecd4922526801a9ad96cd9bd9f9ebe2cc
Tomoya Takiwaki <takiwaki.tomoya@nao.ac.jp>
2022-11-29 11:52:08 +0900

    update

--- MHD3Dgpu/MakeSnap.sh
+++ MHD3Dgpu/MakeSnap.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-gscript=$1
+index=$1
+echo "from "$index
+gscript=$2
 echo "use "$gscript
 
-fstfile=`ls -1 ./output/vor*.dat 2>/dev/null | head -1`
+dir=figures
+if [ ! -d ${dir} ]; then
+    mkdir ${dir}
+fi
+
+fstfile=`ls -1 ./output/${index}*.dat 2>/dev/null | head -1`
 echo $fstfile
 declare -i fstnum=`echo  ${fstfile##*/} | tr -cd '0123456789\n' |sed -e 's/^0\+\([0-9]\+\)$/\1/'`
 echo $fstnum
 
-lstfile=`ls -1 ./output/vor*.dat 2>/dev/null | tail -1`
+lstfile=`ls -1 ./output/${index}*.dat 2>/dev/null | tail -1`
 echo $lstfile
 declare -i lstnum=`echo  ${lstfile##*/} | tr -cd '0123456789\n' |sed -e 's/^0\+\([0-9]\+\)$/\1/'`
 echo $lstnum
@@ -16,3 +23,9 @@ echo $lstnum
 for n in $(seq ${fstnum} ${lstnum}); do
     gnuplot -e ifnum=$n $gscript
 done
+
+
+#!/bin/bash
+
+
+
